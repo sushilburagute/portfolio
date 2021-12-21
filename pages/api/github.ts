468cbd8655876845882cffa2867711f7616eda8f
sushilburagute <codetastic101@gmail.com>
2021-12-22 00:46:29 +0530

    Delete: APIs

--- pages/api/github.ts
+++ pages/api/github.ts
@@ -4,9 +4,11 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const userResponse = await fetch('https://api.github.com/users/leerob');
+  const userResponse = await fetch(
+    'https://api.github.com/users/sushilburagute'
+  );
   const userReposResponse = await fetch(
-    'https://api.github.com/users/leerob/repos?per_page=100'
+    'https://api.github.com/users/sushilburagute/repos?per_page=100'
   );
 
   const user = await userResponse.json();
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,12 +1,8 @@
 import Link from 'next/link';
 
 import Analytics from 'components/metrics/Analytics';
-import Newsletter from 'components/metrics/Newsletter';
 import Container from 'components/Container';
 import GitHub from 'components/metrics/Github';
-import Gumroad from 'components/metrics/Gumroad';
-import Unsplash from 'components/metrics/Unsplash';
-import YouTube from 'components/metrics/Youtube';
 import TopTracks from 'components/TopTracks';
 
 export default function Dashboard() {
@@ -32,17 +28,9 @@ export default function Dashboard() {
             </Link>
           </p>
         </div>
-        <div className="flex flex-col w-full">
-          <Unsplash />
-          <YouTube />
-        </div>
         <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 my-2 w-full">
           <Analytics />
           <GitHub />
-        </div>
-        <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 my-2 w-full">
-          <Gumroad />
-          <Newsletter />
         </div>
         <h2 className="font-bold text-3xl tracking-tight mb-4 mt-16 text-black dark:text-white">
           Top Tracks
